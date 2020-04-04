a668689bbf8bd73154d6c10d3605aab46a053e6a
Drusalyator <And-gridin@yandex.ru>
2020-04-04 23:37:59 +0500

    add lecture05

--- src/Lecture05.hs
+++ src/Lecture05.hs
@@ -43,11 +43,12 @@ module Lecture05 where
     https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes#/media/File:Sieve_of_Eratosthenes_animation.gif
 -}
 sieve :: [Integer] -> [Integer]
-sieve xs = error "not implemented"
+sieve (x : xs) = x : sieve (filter (\y -> (mod y x > 0)) xs)
+sieve [] = []
 
 -- Функция, возвращающая n-ое простое число. Для её реализации используйте функцию sieve
 nthPrime :: Int -> Integer
-nthPrime n = error "not implemented"
+nthPrime n = sieve [2..] !! (n - 1)
 
 {-
     Недавно в интервью Forbes с Сергеем Гуриевым Андрей Мовчан решил показать, что он
@@ -71,11 +72,18 @@ nthPrime n = error "not implemented"
 -- Возвращает бесконечный список ВВП на годы и годы вперёд
 -- yearGDP 100 0.1 ~> [100, 100.1, 100.20009(9), 100.3003.., ...]
 yearGDP :: Double -> Double -> [Double]
-yearGDP now percent = error "not implemented"
+yearGDP now percent = iterate (\x -> x * (1 + percent / 100)) now
+
+chinaGDP :: [Double]
+chinaGDP = yearGDP 10000 6
+
+usaGDP :: [Double]
+usaGDP = yearGDP 66000 2
 
 -- Возвращает количество лет, которые нужны Китаю, чтобы догнать США в текущих условиях
 inHowManyYearsChinaWins :: Int
-inHowManyYearsChinaWins = error "not implemented"
+inHowManyYearsChinaWins = 1 + length (takeWhile (\(x, y) -> x < y) (zip chinaGDP usaGDP))
+-- inHowManyYearsChinaWins = length (takeWhile (\(x, y) -> x < y) (zip chinaGDP usaGDP))
 
 {-
   Пусть у нас есть некоторая лента событий, каждое сообщение в которой говорит,
@@ -115,7 +123,16 @@ allCountries =
   , Country "USA" 0
   , Country "GreatBritain" 0 ]
 
+isSameCountry :: String -> Country -> Bool
+isSameCountry currentName (Country name _) = currentName  == name
+
+getSickCount :: Country -> Integer
+getSickCount (Country _ count) = count
+
+getSickSum :: String -> [Country] -> Integer
+getSickSum countryName events = sum (map getSickCount (filter (isSameCountry countryName) events))
+
 stat :: [Country] -> [Country]
-stat events = error "not implemented"
+stat events = map (\(Country name _) -> Country name (getSickSum name events)) allCountries
 
 -- </Задачи для самостоятельного решения>
